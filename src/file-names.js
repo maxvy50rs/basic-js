2dc6b6379ffd65e25c76351e70abf0653de92a4a
maxvy50rs <vysokov.maksim95@yandex.ru>
2023-04-16 12:13:53 +0600

    subtask: fileNames

--- src/file-names.js
+++ src/file-names.js
@@ -15,10 +15,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(input) {
+  let names = [...input];
+  let nameFreq = {};
+  names.forEach((name, idx, arr) => {
+    nameFreq[name] = nameFreq[name] + 1 || 0;
+    if (nameFreq[name] > 0) {
+      arr[idx] += `(${nameFreq[name]})`;
+      nameFreq[arr[idx]] = nameFreq[arr[idx]] + 1 || 0;
+    }
+  });
+  return names;
 }
+
+renameFiles(["file", "file", "image", "file(1)", "file"]);
 
 module.exports = {
   renameFiles
